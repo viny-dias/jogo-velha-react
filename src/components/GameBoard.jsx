7204f710e1c98c952a073b1eb92569231d9781cf
Vinícius <vinydias0509@gmail.com>
2024-05-14 18:06:08 -0300

    setting up the game winner

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,7 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
-
-export default function GameBoard({ onSelectSquare, turns }) {
-    let gameBoard = initialGameBoard
-
-    for (const turn of turns) {
-        const { square, player } = turn
-        const { row, col } = square
-
-        gameBoard[row][col] = player
-    }
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard)
-
-    // function handleSelectSquare(rowIndex, colIndex) {
-    //     setGameBoard((prevGameBoard) => {
-    //         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
-    //         updateBoard[rowIndex][colIndex] = activePlayerSymbol
-    //         return updateBoard
-    //     })
-
-    //     onSelectSquare()
-    // }
-
+export default function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
