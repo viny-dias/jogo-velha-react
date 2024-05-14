71148c5b6892e4c08a8e3ec6fffc5f5c2f394bce
Vinícius <vinydias0509@gmail.com>
2024-05-14 17:29:28 -0300

    configuring buttons to be clicked only once

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -27,12 +27,17 @@ export default function GameBoard({ onSelectSquare, turns }) {
 
     return (
         <ol id="game-board">
-            {gameBoard.map((row, rowIndex) => ( //fix
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                                <button 
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
